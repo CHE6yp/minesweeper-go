ae021dd2b4442a4bb1b16ea8261b2c7b06b3318f
CHE6yp <che6yp@gmail.com>
2019-12-12 01:29:23 +0300

    somewhat of an mvp, not really

--- minesweeper-go.go
+++ minesweeper-go.go
@@ -3,6 +3,7 @@ package main
 import ("fmt"
 	"os"
 	"math/rand"
+	"strconv"
 )
 
 func main(){
@@ -10,9 +11,15 @@ func main(){
 	var s int
 	fmt.Println("Set the map size")
 	fmt.Fscanln(os.Stdin,&s)
-	var mapi = make([][]int,s)
+	var mapi, showfield = make([][]int,s), make([][]int,s)
+	var seeF = make([][]string,s)
 	for i:=0;i<s;i++{
 		mapi[i] = make([]int,s) 
+		showfield[i] = make([]int,s) 
+		seeF[i] = make([]string,s) 
+		for key := range seeF[i]{
+			seeF[i][key] = "*"
+		}
 	}
 	fmt.Println(mapi)
 
@@ -25,7 +32,40 @@ func main(){
 		key = key
 		fmt.Println(value)
 	}
-	// fmt.Println(mapi)
+	fmt.Println()
+	fmt.Println()
+	setFieldShow(&showfield, mapi)
+	for key, value := range showfield {
+		key = key
+		fmt.Println(value)
+	}
+	printSeeF(seeF)
+	dead := false
+	x,y:=0,0
+	for dead != true {
+		fmt.Println("Pick X")
+		fmt.Fscanln(os.Stdin,&x)
+		fmt.Println("Pick Y")
+		fmt.Fscanln(os.Stdin,&y)
+		seeF[x][y], dead = openCell(x,y,showfield,mapi)
+		printSeeF(seeF)
+		if dead {
+			fmt.Println("You are ded!")
+		}
+
+	}
+
+
+}
+
+func printSeeF(sf [][]string){
+	fmt.Println()
+	fmt.Println()
+	/////////
+	for key, value := range sf {
+		key = key
+		fmt.Println(value)
+	}
 }
 
 func setMines(m *[][]int, s *int){
@@ -36,5 +76,69 @@ func setMines(m *[][]int, s *int){
 	*s--
 	if (*s!=0){
 		setMines(&*m,&*s)
+	}
+}
+
+func setFieldShow(sf *[][]int, m [][]int) {
+	for key, value := range m {
+		for key2, value2 := range value {
+			value2 = value2
+			(*sf)[key][key2]=setCellShow(m,key,key2)
+		}
+	}
+	// return m
+}
+
+func setCellShow(m [][]int, x int, y int) int {
+	count := 0
+	if x!=0 {
+		if m[x-1][y]==1{
+			count++ //8
+		}
+		if y!=0 {
+			if m[x-1][y-1]==1{
+				count++ //1
+			}
+		}
+		if y!=len(m[x])-1{
+			if m[x-1][y+1]==1{
+				count++ //7
+			}
+		}
+	}
+	if x!=len(m)-1 {
+		if m[x+1][y]==1{
+			count++ //4
+		}
+		if y!=0 {
+			if m[x+1][y-1]==1{
+				count++ //3
+			}
+		}
+		if y!=len(m[x])-1{
+			if m[x+1][y+1]==1{
+				count++ //5
+			}
+		}
+	}
+	if y!=0 {
+		if m[x][y-1] ==1 {
+			count++ //2
+		}
+	}
+	if y!=len(m[x])-1{
+		if m[x][y+1]==1{
+			count++ //6
+		}
+	}
+
+	return count
+}
+
+func openCell(x int, y int, sf [][]int, m [][]int) (string, bool) {
+	if m[x][y]==1{
+		return "m",true
+	}else {
+		return strconv.Itoa(sf[x][y]), false
 	}
 }
